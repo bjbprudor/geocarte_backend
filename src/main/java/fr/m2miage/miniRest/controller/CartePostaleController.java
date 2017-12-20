0bdef1ddf0a3d3170ee40fbd91aca3aa895f385d
Pierre-Louis <pl.ollivier29@gmail.com>
2017-12-20 21:07:37 +0100

    Creation carte et variante ainsi que sauvegarde d'une image

--- src/main/java/fr/m2miage/miniRest/controller/CartePostaleController.java
+++ src/main/java/fr/m2miage/miniRest/controller/CartePostaleController.java
@@ -1,9 +1,13 @@
 package fr.m2miage.miniRest.controller;
 
-import fr.m2miage.miniRest.model.CartePostale;
+import fr.m2miage.miniRest.model.*;
 import fr.m2miage.miniRest.repository.CartePostaleRepository;
+import fr.m2miage.miniRest.repository.CommuneRepository;
+import fr.m2miage.miniRest.repository.EditeurRepository;
+import fr.m2miage.miniRest.repository.VarianteCarteRepository;
 import fr.m2miage.miniRest.services.CartePostaleService;
 import fr.m2miage.miniRest.util.CustomErrorType;
+import fr.m2miage.miniRest.util.ImageConverter;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class CartePostaleController
@@ -25,6 +30,15 @@ public class CartePostaleController
 
     @Autowired
     private CartePostaleRepository repo;
+
+    @Autowired
+    private EditeurRepository editeurRepository;
+
+    @Autowired
+    private CommuneRepository communeRepository;
+
+    @Autowired
+    private VarianteCarteRepository varianteCarteRepository;
 
     // -------------------Recupere tous les CartePostales---------------------------------------------
     @CrossOrigin(origins = "http://localhost:4200")
@@ -130,21 +144,46 @@ public class CartePostaleController
     // -------------------Create a CartePostale-------------------------------------------
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/cartePostale/", method = RequestMethod.POST)
-    public ResponseEntity<?> createCartePostale(@RequestBody CartePostale target, UriComponentsBuilder ucBuilder)
+    public ResponseEntity<?> createCartePostale(@RequestBody CartePostalePost target)
     {
         String msg = String.format("Creating CartePostale : {%s}", target);
         log.info(msg);
-        CartePostale current = repo.findOne(target.getId());
-        if (current != null)
+
+        Editeur editeur = editeurRepository.findOne(target.getEditeurId());
+        Commune commune = communeRepository.findOne(target.getInsee());
+        CartePostale cp = new CartePostale();
+        cp.setId(9999);
+        cp.setLatitude(target.getLatitude());
+        cp.setLongitude(target.getLongitude());
+        cp.setCommune(commune);
+        cp.setEditeur(editeur);
+        CartePostale newCp = repo.save(cp);
+
+        VarianteCarteId varId = new VarianteCarteId();
+        varId.setId(1);
+        varId.setCartePostale(newCp);
+        VarianteCarte var = new VarianteCarte();
+        var.setId(varId);
+        var.setLegende(target.getLegende());
+
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(1000);
+        String photoName = editeur.getCode()+randomInt+".jpg";
+        String photoDestination = "src/img/destination";
+
+        var.setFace(photoDestination+photoName);
+
+        ImageConverter.base64ToImage(photoDestination, photoName,target.getBase64());
+
+        /*if (cp != null)
         {
             msg = String.format("Unable to create. A CartePostale with id {%s} already exist", target.getId());
             log.error(msg);
             return new ResponseEntity(new CustomErrorType(msg),HttpStatus.CONFLICT);
-        }
-        repo.save(target);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/cartePostale/{id}").buildAndExpand(target.getId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+        }*/
+
+        varianteCarteRepository.save(var);
+        return new ResponseEntity<>(cp, HttpStatus.CREATED);
     }
 
     // ------------------- Update a CartePostale ------------------------------------------------
---
+++ src/main/java/fr/m2miage/miniRest/model/CartePostalePost.java
@@ -0,0 +1,70 @@
+package fr.m2miage.miniRest.model;
+
+public class CartePostalePost {
+    Integer codeEditeur;
+    String insee;
+    Integer editeurId;
+    Float latitude;
+    Float longitude;
+    String base64;
+    String legende;
+
+    public CartePostalePost() {
+    }
+
+    public Integer getCodeEditeur() {
+        return codeEditeur;
+    }
+
+    public void setCodeEditeur(Integer codeEditeur) {
+        this.codeEditeur = codeEditeur;
+    }
+
+    public String getInsee() {
+        return insee;
+    }
+
+    public void setInsee(String insee) {
+        this.insee = insee;
+    }
+
+    public Integer getEditeurId() {
+        return editeurId;
+    }
+
+    public void setEditeurId(Integer editeurId) {
+        this.editeurId = editeurId;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public void setBase64(String base64) {
+        this.base64 = base64;
+    }
+
+    public String getLegende() {
+        return legende;
+    }
+
+    public void setLegende(String legende) {
+        this.legende = legende;
+    }
+}
