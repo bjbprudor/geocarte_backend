004f7ba62d4d606ee01b5b54bf0c37f3e91acb11
Pierre-Louis <pl.ollivier29@gmail.com>
2017-12-20 02:54:36 +0100

    Ajout de la requête permettant d'avoir la liste d'id utilisateur par variantes

--- src/main/java/fr/m2miage/miniRest/services/CarteUtilisateurService.java
+++ src/main/java/fr/m2miage/miniRest/services/CarteUtilisateurService.java
@@ -1,11 +1,14 @@
 package fr.m2miage.miniRest.services;
 
+import fr.m2miage.miniRest.model.CarteUtilisateurId;
 import fr.m2miage.miniRest.repository.CartePostaleRepository;
 import fr.m2miage.miniRest.repository.CarteUtilisateurRepository;
 import fr.m2miage.miniRest.repository.UtilisateurRepository;
 import fr.m2miage.miniRest.repository.VarianteCarteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("carteUtilisateurService")
 public class CarteUtilisateurService
@@ -24,4 +27,16 @@ public class CarteUtilisateurService
     private CartePostaleRepository cpRepo;
 
 
+    public List<Integer> getUsersIdByCp(Integer cpId){
+        List<Integer> result = null;
+        try
+        {
+            result = repo.findUsersIdByCp(cpId);
+        }
+        catch (Exception ex)
+        {
+
+        }
+        return result;
+    }
 }
